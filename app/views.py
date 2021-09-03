6af51be17f7d4ede2a7e4c2ddd5fe9cec7a9decc
asraful islam <asrafulislam1515k@gmail.com>
2021-09-03 17:02:32 +0600

    registration updated

--- app/views.py
+++ app/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Cart,Customer,Product,OrderPlaced
+from .forms import CustomarRegistrationForm
+from django.contrib import messages
 
-#def home(request):
-#return render(request, 'app/home.html')
+
 class ProductView(View):
   def get(self,request):
    topwearsmale=Product.objects.filter(category='Top Wear Male')
    topwearsfemale=Product.objects.filter(category='Top Wear Female')
    return render(request,'app/home.html',{'topwearsmale':topwearsmale,'topwearsfemale':topwearsfemale})
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+class ProductDetailView(View):
+ def get(self,request,pk):
+  product=Product.objects.get(pk=pk)
+  return render(request, 'app/productdetail.html',{'product':product})
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
@@ -37,8 +40,20 @@ def mobile(request):
 def login(request):
  return render(request, 'app/login.html')
 
-def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+#def customerregistration(request):
+ #return render(request, 'app/customerregistration.html')
+
+class CustomerRegistrationView(View):
+ def get(self,request):
+  form =CustomarRegistrationForm()
+  return render(request,'app/customerregistration.html', {'form':form})
+
+ def post(self,request):
+  form=CustomarRegistrationForm(request.POST)
+  if form.is_valid():
+   messages.success(request, 'Congratulation!! Registered Successfully')
+   form.save()
+  return render(request, 'app/customerregistration.html', {'form':form})
 
 def checkout(request):
  return render(request, 'app/checkout.html')
