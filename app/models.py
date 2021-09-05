6dc3da0bb891547ad3e3c5b0c67ae20fbff7fec0
Ankita Paul <18101091@uap-bd.edu>
2021-09-05 15:51:57 +0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,30 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator,MinValueValidator
+#from django.core.validators import MaxValueValidator, MinValueValidator
 
 DIVISION_CHOICE = (
-    ('Dhaka','Dhaka'),
-    ('Khulna','khulna'),
-    ('Rangpur','Rangpur'),
-    ('Chottogram','Chottogram'),
+    ('Dhaka', 'Dhaka'),
+    ('Khulna', 'khulna'),
+    ('Rangpur', 'Rangpur'),
+    ('Chottogram', 'Chottogram'),
     ('Cumilla', 'Cumilla'),
-    ('Rajshahi','Rajshahi'),
-    ('Sylhet','Sylhet'),
-    ('Borishal','Borishal'),
+    ('Rajshahi', 'Rajshahi'),
+    ('Sylhet', 'Sylhet'),
+    ('Borishal', 'Borishal'),
 )
 class Customer(models.Model):
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     locality = models.CharField(max_length=200)
     city = models.CharField(max_length=50)
     zipcode = models.IntegerField()
-    division = models.CharField(choices=DIVISION_CHOICE,max_length=50)
+    division = models.CharField(choices=DIVISION_CHOICE, max_length=50)
+
 
     def __str__(self):
         return str(self.id)
 
-
 CATEGORY_CHOICES=(
+    
      ('Top Wear Male','Top War Male'),
      ('Top Wear Female','Top War Female'),
  )
